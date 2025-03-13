41ca139c2c19d2b993da6e128aa4bf035dca124f
thibault.duforest <thibault.duforest@gmail.com>
2025-03-13 22:25:46 +0100

    Add basic layout and pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+
+	import { i18n } from '$lib/i18n';
+	import Nav from '$lib/Nav.svelte';
 	import '../app.css';
 	
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	{@render children()}
+	<div class="flex flex-col md:flex-row h-screen">
+		<div class="w-full md:w-52 bg-gray-800 text-white p-4 md:h-auto h-15 overflow-hidden">
+			<Nav />
+		</div>
+	
+		<main class="flex-1 p-4 overflow-auto">
+			{@render children()}
+		</main>
+	</div>
 </ParaglideJS>
