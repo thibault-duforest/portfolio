3b49cf96519fe14f53289b3fa534d3c0ab28c233
thibault.duforest <thibault.duforest@gmail.com>
2025-04-08 20:55:10 +0200

    Update layout rendering on tablet

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<nav>
+	<ul class="flex space-x-4 lg:block lg:space-y-4 lg:space-x-0">
+		{#snippet navLink(href: string, label: string, title: string)}
+			<li>
+				<a {href} {title} class="hover:text-gray-300" class:active={page.url.pathname === href}
+					>{label}</a
+				>
+			</li>
+		{/snippet}
+
+		{@render navLink('/', 'Welcome!', 'Return to homepage')}
+		{@render navLink('/about', 'About me', 'Learn more about my profile')}
+		{@render navLink('/portfolio', 'Portfolio', "Look at Thibault Duforest' realisations")}
+		{@render navLink('/contact', 'Contact me', 'Show contact information')}
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,19 @@
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 
 	import { i18n } from '$lib/i18n';
-	import Nav from '$lib/Nav.svelte';
+	import Nav from '$lib/components/Nav.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	<div class="m-auto flex h-screen flex-col md:flex-row">
-		<div class="h-15 w-full overflow-hidden bg-gray-800 p-4 text-white md:h-auto md:w-52">
+	<div class="m-auto flex h-screen flex-col lg:flex-row">
+		<div class="h-15 w-full overflow-hidden bg-gray-800 p-4 text-white md:h-auto lg:w-52">
 			<Nav />
 		</div>
 
-		<main class="flex-1 overflow-auto p-4">
+		<main class="mb:p-4 flex-1 overflow-auto">
 			{@render children()}
 		</main>
 	</div>
