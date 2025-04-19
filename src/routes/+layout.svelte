78e0c7c0ce5cff07e293129adf8a52eceac20cd0
thibault.duforest <thibault.duforest@gmail.com>
2025-04-19 14:34:22 +0200

    Add meta datas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import { page } from '$app/state';
+
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	import * as m from '$lib/paraglide/messages.js';
 	import Nav from '$lib/components/Nav.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>Thibault Duforest - {m.head_title()}</title>
+	<meta name="description" content={m.head_meta_description()} />
+	<meta name="keywords" content={m.head_meta_keywords()} />
+	<meta name="author" content="Thibault Duforest" />
+	<meta name="robots" content="index, follow" />
+	<meta name="theme-color" content="#1e2939" />
+	<link rel="canonical" href="https://thibault-duforest.github.io/portfolio/" />
+	<script type="application/ld+json">
+		{
+		  "@context": "https://schema.org",
+		  "@type": "Person",
+		  "name": "Thibault Duforest",
+		  "url": "https://thibault-duforest.github.io/portfolio/",
+		  "jobTitle": m.head_title()
+		}
+	</script>
+</svelte:head>
 
 <div class="m-auto flex h-screen flex-col lg:flex-row">
 	<div class="h-24 w-full bg-gray-800 p-4 text-white md:h-auto lg:w-52">
